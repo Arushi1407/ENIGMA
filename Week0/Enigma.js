d0d659b2cb4d27b8907d0585cb738398558e3257
Arushi1407 <arushi.world@gmail.com>
2025-05-25 21:43:00 +0530

    Add files via upload

--- Week0/Enigma.js
+++ Week0/Enigma.js
@@ -1,144 +1,142 @@
-/*
- * File: Enigma.js
- * ---------------
- * This program implements a graphical simulation of the Enigma machine.
- */
-
-import "./graphics.js";
-import "./EnigmaConstants.js";
-
-/* Main program */
-
-
-function Enigma() {
-   var enigmaImage = GImage("EnigmaTopView.png");
-   var gw = GWindow(enigmaImage.getWidth(), enigmaImage.getHeight());
-   gw.add(enigmaImage);
-   runEnigmaSimulation(gw);
-
-}
-
-// You are responsible for filling in the rest of the code.  Your
-// implementation of runEnigmaSimulation should perform the
-// following operations:
-//
-// 1. Create an object that encapsulates the state of the Enigma machine.
-// 2. Create and add graphical objects that sit on top of the image.
-// 3. Add listeners that forward mouse events to those objects.
-class EnigmaState {
-   constructor() {
-       this.rotors = [
-           new Rotor(EnigmaConstants.ROTOR_I_WIRING, EnigmaConstants.ROTOR_I_NOTCH),
-           new Rotor(EnigmaConstants.ROTOR_II_WIRING, EnigmaConstants.ROTOR_II_NOTCH),
-           new Rotor(EnigmaConstants.ROTOR_III_WIRING, EnigmaConstants.ROTOR_III_NOTCH)
-       ];
-       this.reflector = EnigmaConstants.REFLECTOR_WIRING;
-       this.plugboard = new Plugboard();
-   }
-
-   processKeyPress(letter) {
-       this.stepRotors();
-       let signal = letter.charCodeAt(0) - 'A'.charCodeAt(0);
-       signal = this.plugboard.process(signal);
-       signal = this.passThroughRotorsForward(signal);
-       signal = this.passThroughReflector(signal);
-       signal = this.passThroughRotorsReverse(signal);
-       signal = this.plugboard.process(signal);
-       return String.fromCharCode('A'.charCodeAt(0) + signal);
-   }
-
-   stepRotors() {
-       let stepRotor3 = true;
-       let stepRotor2 = this.rotors[2].isAtNotch();
-       let stepRotor1 = this.rotors[1].isAtNotch();
-
-       if (stepRotor3) this.rotors[2].step();
-       if (stepRotor2) this.rotors[1].step();
-       if (stepRotor1) this.rotors[0].step();
-   }
-
-   // Additional methods for rotor signal processing...
-}
-class Rotor {
-   constructor(wiring, notch) {
-       this.wiring = wiring;
-       this.notch = notch.charCodeAt(0) - 'A'.charCodeAt(0);
-       this.position = 0;
-   }
-
-   forwardSignal(signal) {
-       let pos = (signal + this.position) % 26;
-       let output = this.wiring.charCodeAt(pos) - 'A'.charCodeAt(0);
-       return (output - this.position + 26) % 26;
-   }
-
-   reverseSignal(signal) {
-       let pos = (signal + this.position) % 26;
-       let input = String.fromCharCode('A'.charCodeAt(0) + pos);
-       let output = this.wiring.indexOf(input);
-       return (output - this.position + 26) % 26;
-   }
-
-   step() { this.position = (this.position + 1) % 26; }
-   isAtNotch() { return this.position === this.notch; }
-}
-
-class Plugboard {
-   constructor() {
-       this.map = Array.from({length: 26}, (_, i) => i);
-   }
-
-   process(signal) { return this.map[signal]; }
-}
-function runEnigmaSimulation(gw) {
-   const enigmaState = new EnigmaState();
-   const lamps = {};
-
-   // Create keys
-   'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').forEach(letter => {
-       const key = new GOval(EnigmaConstants.KEY_RADIUS * 2, EnigmaConstants.KEY_RADIUS * 2);
-       key.setPosition(EnigmaConstants.KEY_POSITIONS[letter].x, EnigmaConstants.KEY_POSITIONS[letter].y);
-       key.setFilled(true);
-       key.setColor(EnigmaConstants.KEY_OFF_COLOR);
-       key.addEventListener("click", () => handleKeyPress(letter, lamps, enigmaState));
-       gw.add(key);
-   });
-
-   // Create lamps
-   'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').forEach(letter => {
-       const lamp = new GOval(EnigmaConstants.LAMP_RADIUS * 2, EnigmaConstants.LAMP_RADIUS * 2);
-       lamp.setPosition(EnigmaConstants.LAMP_POSITIONS[letter].x, EnigmaConstants.LAMP_POSITIONS[letter].y);
-       lamp.setFilled(true);
-       lamp.setColor(EnigmaConstants.LAMP_OFF_COLOR);
-       gw.add(lamp);
-       lamps[letter] = lamp;
-   });
-
-   // Create rotor displays
-   createRotorDisplay(gw, EnigmaConstants.ROTOR1_POSITION, 0, enigmaState);
-   createRotorDisplay(gw, EnigmaConstants.ROTOR2_POSITION, 1, enigmaState);
-   createRotorDisplay(gw, EnigmaConstants.ROTOR3_POSITION, 2, enigmaState);
-}
-
-function createRotorDisplay(gw, position, rotorIndex, enigmaState) {
-   const rotor = enigmaState.rotors[rotorIndex];
-   const compound = new GCompound();
-   const label = new GLabel(String.fromCharCode('A'.charCodeAt(0) + rotor.position));
-   label.setFont("14px Arial");
-   compound.add(new GRect(40, 40));
-   compound.add(label);
-   compound.setPosition(position.x, position.y);
-   compound.addEventListener("click", () => {
-       rotor.position = (rotor.position + 1) % 26;
-       label.setText(String.fromCharCode('A'.charCodeAt(0) + rotor.position));
-   });
-   gw.add(compound);
-}
-function handleKeyPress(letter, lamps, enigmaState) {
-   const encryptedLetter = enigmaState.processKeyPress(letter);
-   const lamp = lamps[encryptedLetter];
-   lamp.setColor(EnigmaConstants.LAMP_ON_COLOR);
-   setTimeout(() => lamp.setColor(EnigmaConstants.LAMP_OFF_COLOR), 500);
-}
-
-
+// Enigma.js - Main simulation
+const EnigmaConstants = {
+    // Rotor configurations
+    ROTOR_I: { wiring: "EKMFLGDQVZNTOWYHXUSPAIBRCJ", notch: "Q" },
+    ROTOR_II: { wiring: "AJDKSIRUXBLHWTMCQGZNPYFVOE", notch: "E" },
+    ROTOR_III: { wiring: "BDFHJLCPRTXVZNYEIWGAKMUSQO", notch: "V" },
+    REFLECTOR: "YRUHQSLDPXNGOKMIEBFZCWVJAT",
+  
+    // UI positions (example coordinates)
+    KEY_POSITIONS: {
+      A: {x: 100, y: 500}, B: {x: 130, y: 500}, C: {x: 160, y: 500},
+      // ... Add positions for all 26 letters
+    },
+    LAMP_POSITIONS: {
+      A: {x: 100, y: 100}, B: {x: 130, y: 100}, C: {x: 160, y: 100},
+      // ... Add positions for all 26 letters
+    },
+    ROTOR_POSITIONS: [{x: 300, y: 250}, {x: 400, y: 250}, {x: 500, y: 250}],
+    KEY_RADIUS: 15,
+    COLORS: {
+      key: "#666666",
+      lamp_off: "#444444",
+      lamp_on: "#FFFF00",
+      rotor_bg: "#DDDDDD"
+    }
+  };
+  
+  class Rotor {
+    constructor(wiring, notch) {
+      this.wiring = wiring;
+      this.notch = notch.charCodeAt(0) - 65;
+      this.position = 0;
+    }
+  
+    forwardSignal(input) {
+      return (this.wiring.charCodeAt((input + this.position) % 26) - 65 - this.position + 26) % 26;
+    }
+  
+    reverseSignal(input) {
+      const index = (this.wiring.indexOf(String.fromCharCode(input + 65)) - this.position + 26) % 26;
+      return index;
+    }
+  
+    step() { this.position = (this.position + 1) % 26; }
+    isAtNotch() { return this.position === this.notch; }
+  }
+  
+  class EnigmaMachine {
+    constructor() {
+      this.rotors = [
+        new Rotor(EnigmaConstants.ROTOR_I.wiring, EnigmaConstants.ROTOR_I.notch),
+        new Rotor(EnigmaConstants.ROTOR_II.wiring, EnigmaConstants.ROTOR_II.notch),
+        new Rotor(EnigmaConstants.ROTOR_III.wiring, EnigmaConstants.ROTOR_III.notch)
+      ];
+      this.reflector = EnigmaConstants.REFLECTOR;
+      this.plugboard = Array.from({length: 26}, (_, i) => i);
+    }
+  
+    encrypt(char) {
+      this.stepRotors();
+      let signal = char.toUpperCase().charCodeAt(0) - 65;
+  
+      // Signal path
+      signal = this.plugboard[signal];
+      for (let i = this.rotors.length - 1; i >= 0; i--) signal = this.rotors[i].forwardSignal(signal);
+      signal = this.reflector.charCodeAt(signal) - 65;
+      for (let i = 0; i < this.rotors.length; i++) signal = this.rotors[i].reverseSignal(signal);
+      signal = this.plugboard[signal];
+  
+      return String.fromCharCode(signal + 65);
+    }
+  
+    stepRotors() {
+      const stepThird = true;
+      const stepSecond = this.rotors[2].isAtNotch();
+      const stepFirst = this.rotors[1].isAtNotch();
+  
+      if (stepThird) this.rotors[2].step();
+      if (stepSecond) this.rotors[1].step();
+      if (stepFirst) this.rotors[0].step();
+    }
+  }
+  
+  function createEnigmaUI(gw) {
+    const enigma = new EnigmaMachine();
+    const lamps = {};
+  
+    // Create keyboard
+    Object.entries(EnigmaConstants.KEY_POSITIONS).forEach(([letter, pos]) => {
+      const key = new GOval(pos.x, pos.y, 
+                          EnigmaConstants.KEY_RADIUS*2, 
+                          EnigmaConstants.KEY_RADIUS*2);
+      key.setFilled(true);
+      key.setColor(EnigmaConstants.COLORS.key);
+      key.addEventListener("click", () => {
+        const result = enigma.encrypt(letter);
+        lamps[result].setColor(EnigmaConstants.COLORS.lamp_on);
+        setTimeout(() => {
+          lamps[result].setColor(EnigmaConstants.COLORS.lamp_off);
+          gw.redraw();
+        }, 300);
+      });
+      gw.add(key);
+    });
+  
+    // Create lamps
+    Object.entries(EnigmaConstants.LAMP_POSITIONS).forEach(([letter, pos]) => {
+      const lamp = new GOval(pos.x, pos.y, 
+                           EnigmaConstants.KEY_RADIUS*2, 
+                           EnigmaConstants.KEY_RADIUS*2);
+      lamp.setFilled(true);
+      lamp.setColor(EnigmaConstants.COLORS.lamp_off);
+      gw.add(lamp);
+      lamps[letter] = lamp;
+    });
+  
+    // Create rotor displays
+    enigma.rotors.forEach((rotor, i) => {
+      const pos = EnigmaConstants.ROTOR_POSITIONS[i];
+      const bg = new GRect(pos.x, pos.y, 40, 40);
+      const label = new GLabel(String.fromCharCode(65 + rotor.position), pos.x + 10, pos.y + 25);
+      bg.setFilled(true);
+      bg.setColor(EnigmaConstants.COLORS.rotor_bg);
+      
+      bg.addEventListener("click", () => {
+        rotor.step();
+        label.setText(String.fromCharCode(65 + rotor.position));
+        gw.redraw();
+      });
+  
+      gw.add(bg);
+      gw.add(label);
+    });
+  }
+  
+  // Initialize simulation
+  function EnigmaApp() {
+    const gw = new GWindow(800, 600);
+    createEnigmaUI(gw);
+  }
+  
+  window.onload = EnigmaApp;
--- Week0/graphics.js
+++ Week0/graphics.js
@@ -1,74 +1,128 @@
-// Custom graphics library for CS106J Assignment 5
+// graphics.js - Custom graphics library
 class GWindow {
-    constructor(width, height) {
-      this.canvas = document.createElement("canvas");
-      this.canvas.width = width;
-      this.canvas.height = height;
-      this.ctx = this.canvas.getContext("2d");
-      this.objects = [];
-      document.body.appendChild(this.canvas);
-    }
-  
-    add(obj) {
-      this.objects.push(obj);
-      this.redraw();
-    }
-  
-    redraw() {
-      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-      this.objects.forEach(obj => obj.draw(this.ctx));
-    }
+  constructor(width, height) {
+    this.canvas = document.createElement("canvas");
+    this.canvas.width = width;
+    this.canvas.height = height;
+    this.ctx = this.canvas.getContext("2d");
+    this.objects = [];
+    document.body.appendChild(this.canvas);
+
+    // Event handling
+    this.canvas.addEventListener("click", (e) => this.handleClick(e));
   }
-  
-  class GObject {
-    constructor(x, y, width, height) {
-      this.x = x;
-      this.y = y;
-      this.width = width;
-      this.height = height;
-      this.listeners = [];
-    }
-  
-    addEventListener(type, callback) {
-      if (type === "click") {
-        this.listeners.push(callback);
+
+  add(obj) {
+    this.objects.push(obj);
+    this.redraw();
+  }
+
+  redraw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.objects.forEach(obj => obj.draw(this.ctx));
+  }
+
+  handleClick(e) {
+    const rect = this.canvas.getBoundingClientRect();
+    const mouseX = e.clientX - rect.left;
+    const mouseY = e.clientY - rect.top;
+
+    this.objects.forEach(obj => {
+      if (obj.contains(mouseX, mouseY)) {
+        obj.listeners.forEach(callback => callback());
       }
-    }
-  
-    contains(mouseX, mouseY) {
-      // Basic bounding box check (override for shapes like oval)
-      return mouseX >= this.x && mouseX <= this.x + this.width &&
-             mouseY >= this.y && mouseY <= this.y + this.height;
+    });
+  }
+}
+
+class GObject {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.listeners = [];
+  }
+
+  addEventListener(type, callback) {
+    if (type === "click") this.listeners.push(callback);
+  }
+
+  contains(mouseX, mouseY) {
+    return mouseX >= this.x && mouseX <= this.x + this.width &&
+           mouseY >= this.y && mouseY <= this.y + this.height;
+  }
+}
+
+class GRect extends GObject {
+  constructor(x, y, width, height) {
+    super(x, y, width, height);
+    this.color = "black";
+    this.filled = false;
+  }
+
+  setColor(color) { this.color = color; }
+  setFilled(filled) { this.filled = filled; }
+
+  draw(ctx) {
+    ctx.fillStyle = this.color;
+    if (this.filled) {
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    } else {
+      ctx.strokeRect(this.x, this.y, this.width, this.height);
     }
   }
-  
-  class GOval extends GObject {
-    constructor(x, y, width, height) {
-      super(x, y, width, height);
-      this.color = "black";
-      this.filled = false;
-    }
-  
-    setColor(color) { this.color = color; }
-    setFilled(filled) { this.filled = filled; }
-  
-    draw(ctx) {
-      ctx.beginPath();
-      ctx.ellipse(this.x + this.width/2, this.y + this.height/2, 
-                  this.width/2, this.height/2, 0, 0, Math.PI * 2);
-      ctx.fillStyle = this.color;
-      if (this.filled) ctx.fill();
-      ctx.stroke();
-    }
-  
-    contains(mouseX, mouseY) {
-      // Oval collision check (simplified)
-      const centerX = this.x + this.width/2;
-      const centerY = this.y + this.height/2;
-      const dx = mouseX - centerX;
-      const dy = mouseY - centerY;
-      return (dx*dx)/((this.width/2)**2) + (dy*dy)/((this.height/2)**2) <= 1;
-    }
+}
+
+class GOval extends GObject {
+  constructor(x, y, width, height) {
+    super(x, y, width, height);
+    this.color = "black";
+    this.filled = false;
   }
-  
-  // Add similar classes for GRect, GLabel, GImage as needed
+
+  setColor(color) { this.color = color; }
+  setFilled(filled) { this.filled = filled; }
+
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.ellipse(
+      this.x + this.width/2,
+      this.y + this.height/2,
+      this.width/2,
+      this.height/2,
+      0, 0, Math.PI * 2
+    );
+    ctx.fillStyle = this.color;
+    if (this.filled) ctx.fill();
+    ctx.stroke();
+  }
+
+  contains(mouseX, mouseY) {
+    const centerX = this.x + this.width/2;
+    const centerY = this.y + this.height/2;
+    const dx = mouseX - centerX;
+    const dy = mouseY - centerY;
+    return (dx ** 2)/((this.width/2) ** 2) + (dy ** 2)/((this.height/2) ** 2) <= 1;
+  }
+}
+
+class GLabel extends GObject {
+  constructor(text, x, y) {
+    super(x, y, 0, 0);
+    this.text = text;
+    this.color = "black";
+    this.font = "14px Arial";
+  }
+
+  setText(text) { this.text = text; }
+  setFont(font) { this.font = font; }
+
+  draw(ctx) {
+    ctx.fillStyle = this.color;
+    ctx.font = this.font;
+    ctx.fillText(this.text, this.x, this.y);
+  }
+
+  contains() { return false; } // Labels aren't clickable
+}
